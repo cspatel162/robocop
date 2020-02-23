cb67fa470c5728628630e9f565059c4d21357410
rupankarghosh <rupankar326@gmail.com>
2020-02-23 22:50:30 +0530

    change in the programm to save outputs as text

--- robocop.py
+++ robocop.py
@@ -12,11 +12,13 @@ import colorama
 # sexify the terminal with colorama
 # creating color classes
 
+
 class colors():
     redc = colorama.Fore.RED
     bluec = colorama.Fore.BLUE
     greenc = colorama.Fore.GREEN
     yellowc = colorama.Fore.YELLOW
+
 
 parser = argparse.ArgumentParser()
 # url to look for the robots.txt
@@ -25,6 +27,10 @@ parser.add_argument('-o', '--output', required=False,
                     help="option for saving the result into a file")
 
 args = parser.parse_args()
+
+
+# creating empty file for output result
+nfile = open(args.url, "w+")
 
 # making the complete url
 fullUrl = "https://" + args.url + "/robots.txt"
